07367b2d9831e21b4be93df29e2d7821cef70bd7
Jon Veach <veach09@gmail.com>
2019-10-22 13:36:47 -0700

    Created 3 pages, main, addTask and allTasks

---
+++ app/src/main/java/com/zaffierce/taskplanner/AddTaskActivity.java
@@ -0,0 +1,14 @@
+package com.zaffierce.taskplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addtask);
+    }
+}
--- app/src/main/java/com/zaffierce/taskplanner/MainActivity.java
+++ app/src/main/java/com/zaffierce/taskplanner/MainActivity.java
@@ -2,7 +2,9 @@ package com.zaffierce.taskplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addTaskButton = findViewById(R.id.addTaskButton);
+        addTaskButton.setOnClickListener((event) -> {
+            startActivity(new Intent(MainActivity.this, AddTaskActivity.class));
+        });
+
+        Button allTaskButton = findViewById(R.id.allTasksButton);
+        allTaskButton.setOnClickListener((event) -> {
+            startActivity(new Intent(MainActivity.this, allactivity.class));
+        });
+
+
     }
+
+
 }
